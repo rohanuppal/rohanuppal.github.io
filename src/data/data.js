b053d1dc04c692146ab9bba67f9df629fd8215b6
Rohan Uppal <48536081+rohanuppal@users.noreply.github.com>
2023-07-27 12:53:26 -0400

    Add files via upload

---
+++ src/data/data.js
@@ -0,0 +1,30 @@
+import { AiFillAppstore } from "react-icons/ai";
+import {  FaBrain} from "react-icons/fa";
+import { SiProgress} from "react-icons/si";
+import { CgWebsite } from "react-icons/cg";
+import { MdAnalytics } from "react-icons/md";
+
+
+// Features Data
+export const featuresData = [
+  {
+    id: 1,
+    title: "Project Management",
+    des: "Want to gain real-life experience of managing a complete software project cycle.",
+  },
+  {
+    id: 2,
+    icon: <AiFillAppstore />,
+    title: "App Development",
+    des: "Will learn this soon in near future !",
+  },
+  {
+    id: 3,
+    icon: <SiProgress />,
+    title: "SEO Optimisation",
+    des: "Will learn this soon in near future !",
+  }
+];
+
+
+
