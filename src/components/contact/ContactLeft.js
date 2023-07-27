4e58155135fa7c1ddffdef715c28358f68bb4822
Rohan Uppal <48536081+rohanuppal@users.noreply.github.com>
2023-07-27 16:52:53 -0400

    Update ContactLeft.js

--- src/components/contact/ContactLeft.js
+++ src/components/contact/ContactLeft.js
@@ -1,51 +1,52 @@
-import React from 'react'
-import {  FaLinkedinIn, FaMedium } from "react-icons/fa";
-import { SiGithub,SiGmail } from "react-icons/si";
+import React from 'react';
+import { FaLinkedinIn, FaMedium } from "react-icons/fa";
+import { SiGithub, SiGmail } from "react-icons/si";
 import { contactImg } from "../../assets/index";
-
 
 const ContactLeft = () => {
   return (
-    <div className="w-full lgl:w-[35%] h-full bg-gradient-to-r from-[#1e2024] to-[#23272b] p-4 lgl:p-8 rounded-lg shadow-shadowOne flex flex-col gap-5 justify-center">
-      <img
-        className="w-full h-64 object-cover rounded-lg mb-2"
-        src={contactImg}
-        alt="contactImg"
-      />
-      <div className="flex flex-col gap-1">
-        <h3 className="text-3xl font-bold text-white">Rohan Uppal</h3>
-        <p className="text-lg font-normal text-gray-400">
-          Computer Engineering Student
-        </p>
-        <p className="text-base text-gray-400 tracking-wide">
-        I'm an undergraduate student at Toronto Metropolitan University (Ryerson) studying Computer Engineering. I have a deep interest in programming, software design, cloud computing, machine learning, AR/VR, cybersecurity, and DevOps.
-        </p>
-      </div>
-      <div className="flex flex-col gap-0">
-        <h2 className="text-base uppercase font-titleFont mb-4">Find me on</h2>
-        <div className="flex gap-4">
-          <span className="bannerIcon">
-          <a href="https://github.com/rohanuppal" target="_blank" rel="noopener noreferrer">
-            <SiGithub /></a>
-          </span>
-          <span className="bannerIcon">
-          <a href="mailto:rohan.uppal@hotmail.com">
-            <SiGmail /></a>
-          </span>
-          <span className="bannerIcon">
-          <a href="https://www.linkedin.com/in/rohan-uppal-756654171/" target="_blank" rel="noopener noreferrer">
-            <FaLinkedinIn /></a>
-          </span>
-          <span className="bannerIcon">
-          <a href="https://medium.com/@rohanuppal35" target="_blank" rel="noopener noreferrer">
-            <FaMedium /></a>
-          </span>
+    <div className="w-full bg-gradient-to-r from-[#1e2024] to-[#23272b] p-4 lgl:p-8 rounded-lg shadow-shadowOne flex justify-center items-center h-full">
+      <div>
+        <div className="flex flex-col items-center mb-16">
+          <h3 className="text-3xl font-bold text-grey">FIND ME ON</h3>
+        </div>
+        <div className="flex gap-32">
+          <div className="flex flex-col items-center">
+            <span className="bannerIcon">
+              <a href="https://github.com/rohanuppal" target="_blank" rel="noopener noreferrer">
+                <SiGithub />
+              </a>
+            </span>
+            <a className="text-white mt-2" href="https://github.com/rohanuppal" target="_blank" rel="noopener noreferrer">GitHub</a>
+          </div>
+          <div className="flex flex-col items-center">
+            <span className="bannerIcon">
+              <a href="mailto:rohan.uppal@hotmail.com">
+                <SiGmail />
+              </a>
+            </span>
+            <a className="text-white mt-2">rohan.uppal@hotmail.com</a>
+          </div>
+          <div className="flex flex-col items-center">
+            <span className="bannerIcon">
+              <a href="https://www.linkedin.com/in/rohan-uppal-756654171/" target="_blank" rel="noopener noreferrer">
+                <FaLinkedinIn />
+              </a>
+            </span>
+            <a className="text-white mt-2" href="https://www.linkedin.com/in/rohan-uppal-756654171/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+          </div>
+          <div className="flex flex-col items-center">
+            <span className="bannerIcon">
+              <a href="https://medium.com/@rohanuppal35" target="_blank" rel="noopener noreferrer">
+                <FaMedium />
+              </a>
+            </span>
+            <a className="text-white mt-2" href="https://medium.com/@rohanuppal35" target="_blank" rel="noopener noreferrer">Medium</a>
+          </div>
         </div>
       </div>
     </div>
   );
 }
 
-
-export default ContactLeft
-
+export default ContactLeft;
