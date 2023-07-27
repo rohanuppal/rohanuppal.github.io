9febe8d06fada186d7a80f2c9b1f79bc784d7107
Rohan Uppal <48536081+rohanuppal@users.noreply.github.com>
2023-07-27 15:46:22 -0400

    Update Experience.js

--- src/components/resume/Experience.js
+++ src/components/resume/Experience.js
@@ -14,19 +14,22 @@ const Experience = () => {
         <div className="mt-14 w-[585px] h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
           <ResumeCard
             title="Technology Analyst Intern"
-            subTitle="Deloitte (May 2023 - Present)"
+            subTitle1="Deloitte"
+            subTitle2="May 2023 - Present"
             result="TORONTO"
-            des="Developed webpages, maintained SQL databases, and conduct ediscovery and computer forensics."
+            des="Developed webpages, maintained SQL databases, and conducted e-discovery and computer forensics."
           />
           <ResumeCard
             title="Customer Service Rep."
-            subTitle="Home Depot (May 2021 - Jan 2023)"
+            subTitle1="Home Depot"
+            subTitle2="May 2021 - Jan 2023"
             result="MARKHAM"
             des="Customer service with exceptional communication skills assisting customers proven by; resolving customer complaints and fostering a welcoming environment."
           />
           <ResumeCard
             title="Student Host"
-            subTitle="Ontario Science Centre (Sep 2019 - Feb 2020)"
+            subTitle1="Ontario Science Centre"
+            subTitle2="Sep 2019 - Feb 2020"
             result="TORONTO"
             des="Ran activities for visitors and assisted scientists in school programs."
           />
@@ -36,19 +39,22 @@ const Experience = () => {
         <div className="mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
           <ResumeCard
             title="Computer Eng. Intern"
-            subTitle="Ontario Power Generation (May 2022 - Dec 2022)"
+            subTitle1="Ontario Power Generation"
+            subTitle2="May 2022 - Dec 2022"
             result="BOWMANVILLE"
             des="Built PowerBI reports, created PowerApps, and worked on projects involving databases."
           />
           <ResumeCard
             title="Swimming Instructor"
-            subTitle="City of Markham (Jul 2019 - Jul 2020)"
+            subTitle1="City of Markham"
+            subTitle2="Jul 2019 - Jul 2020"
             result="MARKHAM"
             des="Lead structured swimming classes for various age groups and provided feedback to parents."
           />
           <ResumeCard
             title="Innovator"
-            subTitle="The Knowledge Society (TKS) (Aug 2018 - Aug 2019)"
+            subTitle1="The Knowledge Society (TKS)"
+            subTitle2="Aug 2018 - Aug 2019"
             result="TORONTO"
             des="The Knowledge Society exposes young people to cutting-edge technologies."
           />
