95dcd68b202a1af3e28f99216e039e773a2fcd48
Rohan Uppal <48536081+rohanuppal@users.noreply.github.com>
2023-07-27 12:47:53 -0400

    Add files via upload

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { FaLinkedinIn } from "react-icons/fa";
+import { SiGithub, SiGmail } from "react-icons/si";
+import { logo } from "../../assets/index"
+
+
+const Footer = () => {
+ 
+}
+
+
+export default Footer
+
