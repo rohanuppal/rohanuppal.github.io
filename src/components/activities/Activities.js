bae36b2f18ce9a24711dc254052bb2fe78f2e147
Rohan Uppal <48536081+rohanuppal@users.noreply.github.com>
2023-07-28 11:07:52 -0400

    Update Activities.js

--- src/components/activities/Activities.js
+++ src/components/activities/Activities.js
@@ -8,7 +8,7 @@ const Activities = () => {
       id="activities"
       className="w-full py-20 border-b-[1px] border-b-black"
     >
-      <Title des="Activites" />
+      <Title des="Activities" />
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-20">
         {featuresData.map((item) => (
           <Card item={item} key={item.id} />
