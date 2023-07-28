beba8ff8b056dda26a0a18010c4193d4bc29cbd4
Rohan Uppal <48536081+rohanuppal@users.noreply.github.com>
2023-07-28 14:12:36 -0400

    Update Navbar.js

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -10,10 +10,7 @@ import { navLinksdata } from '../../constants';
 const Navbar = () => {
   const [showMenu, setShowMenu]=useState(false)
   return (
-    <div className="w-full h-24 sticky top-0 z-50 bg-bodyColor mx-auto flex justify-between items-center font-titleFont border-b-[1px] border-b-gray-600">
-      <div>
-        <img src={logo} alt="logo" />
-      </div>
+    <div className="w-full h-24 sticky top-0 z-50 bg-bodyColor mx-auto flex justify-center items-center font-titleFont border-b-[1px] border-b-gray-600">
       <div>
         <ul className="hidden mdl:inline-flex items-center gap-6 lg:gap-10">
           {navLinksdata.map(({ _id, title, link }) => (
