4ea9a1081df8be38ce8e1c74c0fee8cde38a15fb
Rohan Uppal <48536081+rohanuppal@users.noreply.github.com>
2023-07-28 14:27:47 -0400

    Update Education.js

--- src/components/resume/Education.js
+++ src/components/resume/Education.js
@@ -15,22 +15,22 @@ const Education = () => {
         <div className="py-6 lgl:py-12 font-titleFont flex flex-col gap-4">
           <h2 className="text-3xl md:text-4xl font-bold">Education</h2>
         </div>
-        <div className="mt-6 lgl:mt-4 w-[585px] h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
+        <div className="mt-6 lgl:mt-4 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
           <ResumeCard
             title="B Eng. - Computer Engineering"
-            subTitle="Toronto Metropolitan University (2020 - 2024)"
+            subTitle1="Toronto Metropolitan University (2020 - 2024)"
             result="3.5 GPA"
             des="Relevant Courses: Data Algorithms, Software Design, Computer Architecture, Operating Systems"
           />
           <ResumeCard
             title="Grade 12"
-            subTitle="Ontario Science Centre Science School (2019 - 2020)"
+            subTitle1="Ontario Science Centre Science School (2019 - 2020)"
             result="HONOR ROLL"
             des="1 of 30 students from all over Ontario to study at the OSCSS."
           />
           <ResumeCard
             title="Grade 9 - 11"
-            subTitle="Bur Oak Secondary School (2016 - 2020)"
+            subTitle1="Bur Oak Secondary School (2016 - 2020)"
             result="HONOR ROLL"
             des="Studied in this school for grades 9 to 11."
           />
@@ -43,17 +43,17 @@ const Education = () => {
         <div className="mt-6 lgl:mt-4 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
           <ResumeCard
             title="Intermediate SQL for Data Analysis Course"
-            subTitle="Dataquest.io (Feb 2023)"
+            subTitle1="Dataquest.io (Feb 2023)"
             des="Learnt SQL for Data Analysis."
           />
           <ResumeCard
             title=" Machine Learning in Python"
-            subTitle="Stanford University (Jun 2021)"
+            subTitle1="Stanford University (Jun 2021)"
             des="A machine learning course done using Python."
           />
           <ResumeCard
             title="Orange Badge I"
-            subTitle="Ontario Power Generation (Jun 2022)"
+            subTitle1="Ontario Power Generation (Jun 2022)"
             des="Certificate for nuclear power plants."
           />
         </div>
